7546a2b60abc7dab4b15ae1eae971e3259625fad
Edwin Khoo <edwinksl@gmail.com>
2016-06-12 19:34:09 -0400

    Add code to find entries of 2 vectors that minimize their difference

---
+++ stack-overflow/differences_of_pairs_of_vectors.m
@@ -0,0 +1,12 @@
+% Code for http://stackoverflow.com/a/37773307/486919
+
+close all
+clc
+
+A = [2.29 2.56 2.77 2.90 2.05];
+B = [2.34 2.62 2.67 2.44 2.52];
+C = abs(bsxfun(@minus,A.',B));
+[~,I] = min(C(:));
+[a,b] = ind2sub(size(C),I);
+A(a)
+B(b)
