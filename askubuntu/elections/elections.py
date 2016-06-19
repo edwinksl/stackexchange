e52c91485865d08ff33f6608e046b8fe82a18ef5
Edwin Khoo <edwinksl@gmail.com>
2016-06-19 01:13:59 -0400

    Parse HTML file for usernames and votes

---
+++ askubuntu/elections/elections.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from bs4 import BeautifulSoup
+# import requests
+
+url = 'https://askubuntu.com/election/5?tab=primary'
+
+# r = requests.get(url)
+# soup = BeautifulSoup(r.text, 'html.parser')
+
+soup = BeautifulSoup(open('elections.html'), 'html.parser')
+
+user_details = soup.select('.user-details')
+usernames = [i.find('a').string for i in user_details]
+
+vote_count_post = soup.select('.vote-count-post')
+votes = [i.string for i in vote_count_post]
+
+table = dict(zip(usernames, votes))
+table
