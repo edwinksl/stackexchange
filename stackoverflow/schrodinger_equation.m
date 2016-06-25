e9fcacd9d59377091d2bb2111325444b1e543383
Edwin Khoo <edwinksl@gmail.com>
2016-06-25 05:58:07 -0400

    Modify H

--- stackoverflow/schrodinger_equation.m
+++ stackoverflow/schrodinger_equation.m
@@ -7,17 +7,25 @@ clc
 
 psi0 = [0;1];
 hbar = 1;
-t = 0:1:100;
-[T,psi] = ode45(@(t,psi)dpsi(t,psi,hbar),t,psi0);
+% t = 0:1:100;
+% t = [0 100];
+t = [0 10];
+% [T,psi] = ode45(@(t,psi)dpsi(t,psi,hbar),t,psi0);
+[T,psi] = ode15s(@(t,psi)dpsi(t,psi,hbar),t,psi0);
 
 for i = 1:length(psi0)
   figure
   plot(T,real(psi(:,i)),T,imag(psi(:,i)))
-  legend('Re','Im')
+  legend('Re','Im','Location','best')
 end
 
 end
 
 function rhs = dpsi(t,psi,hbar)
-  rhs = 1/(1i*hbar)*cos(t).*ones(2,1);
+  % rhs = 1/(1i*hbar)*cos(t).*ones(2,1);
+  rhs = NaN(2,1);
+  rhs(1) = 0.3*t*psi(1) + 1.2*t*psi(2);
+  rhs(2) = t*psi(1) + t^2*psi(2);
+
+  rhs = 1/(1i*hbar)*cos(t).*rhs;
 end
