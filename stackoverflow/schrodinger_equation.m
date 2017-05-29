468cded2bd1c95fafc52d9bac1ef159442a1c5f0
Edwin Khoo <edwinksl@gmail.com>
2017-05-29 17:23:46 -0400

    Remove unused code and refactor code

--- stackoverflow/schrodinger_equation.m
+++ stackoverflow/schrodinger_equation.m
@@ -4,25 +4,19 @@ function schrodinger_equation
 
 psi0 = [0;1];
 hbar = 1;
-% t = 0:1:100;
-% t = [0 100];
-t = [0 10];
-% [T,psi] = ode45(@(t,psi)dpsi(t,psi,hbar),t,psi0);
+t = [0 100];
 [T,psi] = ode15s(@(t,psi)dpsi(t,psi,hbar),t,psi0);
 
 for i = 1:length(psi0)
   figure
   plot(T,real(psi(:,i)),T,imag(psi(:,i)))
-  legend('Re','Im','Location','best')
+  xlabel('t')
+  ylabel('Re(\psi) or Im(\psi)')
+  legend('Re(\psi)','Im(\psi)','Location','best')
 end
 
 end
 
 function rhs = dpsi(t,psi,hbar)
-  % rhs = 1/(1i*hbar)*cos(t).*ones(2,1);
-  rhs = NaN(2,1);
-  rhs(1) = 0.3*t*psi(1) + 1.2*t*psi(2);
-  rhs(2) = t*psi(1) + t^2*psi(2);
-
-  rhs = 1/(1i*hbar)*cos(t).*rhs;
+  rhs = 1/(1i*hbar)*cos(t).*ones(2,1);
 end
