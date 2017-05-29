b539ab403421df682c925e3e2eca1a30bcd86f92
Edwin Khoo <edwinksl@gmail.com>
2017-05-29 17:24:06 -0400

    Fix indentation

--- stackoverflow/schrodinger_equation.m
+++ stackoverflow/schrodinger_equation.m
@@ -2,18 +2,18 @@
 
 function schrodinger_equation
 
-psi0 = [0;1];
-hbar = 1;
-t = [0 100];
-[T,psi] = ode15s(@(t,psi)dpsi(t,psi,hbar),t,psi0);
+  psi0 = [0;1];
+  hbar = 1;
+  t = [0 100];
+  [T,psi] = ode15s(@(t,psi)dpsi(t,psi,hbar),t,psi0);
 
-for i = 1:length(psi0)
-  figure
-  plot(T,real(psi(:,i)),T,imag(psi(:,i)))
-  xlabel('t')
-  ylabel('Re(\psi) or Im(\psi)')
-  legend('Re(\psi)','Im(\psi)','Location','best')
-end
+  for i = 1:length(psi0)
+    figure
+    plot(T,real(psi(:,i)),T,imag(psi(:,i)))
+    xlabel('t')
+    ylabel('Re(\psi) or Im(\psi)')
+    legend('Re(\psi)','Im(\psi)','Location','best')
+  end
 
 end
 
