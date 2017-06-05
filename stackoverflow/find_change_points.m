c5d53999df3ab3b54f2d460a2ed2a7101e6bcded
Edwin Khoo <edwinksl@gmail.com>
2017-06-05 03:11:47 -0400

    Update URL

--- stackoverflow/find_change_points.m
+++ stackoverflow/find_change_points.m
@@ -1,4 +1,4 @@
-% Code for https://stackoverflow.com/questions/43362831/fit-a-piecewise-regression-in-matlab-and-find-change-point
+% Code for https://stackoverflow.com/a/43461233/486919
 
 data = [1 4 4 3 4 0 0 4 5 4 5 2 5 10 5 1 4 15 4 9 11 16 23 25 24 17 31 42 35 45 49 54 74 69 63 46 35 31 27 15 10 5 10 4 2 4 2 2 3 5 2 2];
 x = 1:52;
